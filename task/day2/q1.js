0c8a189b2ee1afeeee561580ac95091e9b963943
vishmayamaki <maki83148@gmail.com>
2024-02-14 05:27:39 +0530

    initial

---
+++ task/day2/q1.js
@@ -0,0 +1,41 @@
+//write a blog different between doument and window
+
+
+/*1. document object:
+
+
+           1.reporesents the HTML content within abrowser tab or frame 
+           
+           2.provides method to interact with and manipulate elements 
+           within the HTML document
+
+           
+           3.nested withim the window object,focusing on dom manipulation
+           and  document structure
+
+
+
+
+
+    2.window object:
+            
+      
+
+
+               1.Rereporesents thew browser window or taB ENCOMPASSING THE
+                ENTER BROWSEING ENVIRONMENT.
+
+
+
+               2.offers functioonallty relarted to browser behavior,navigation
+                and interacvtion
+
+
+
+               3.top-level object in the browser's javascript object model,
+               controlling browser properties and execusing scripts.
+            
+
+
+
+*/
